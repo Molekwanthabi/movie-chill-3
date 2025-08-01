64564ed654824243f14a46d08050dc71e4696443
Nthabiseng <molekwanthabiseng@gmail.com>
2025-08-01 10:26:14 +0200

    Added message

--- alx-fe-reactjs/alx-react-app/src/App.jsx
+++ alx-fe-reactjs/alx-react-app/src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is My webpage</h1>
-      <h2>Hello Nthabi</h2>
-      <p>Can you please come home</p>
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
