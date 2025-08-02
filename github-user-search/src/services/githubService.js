bd9ebeb0f40f15d1de98d0350e3acf95f5ee4fb5
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-08-02 16:41:57 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,22 @@
+import axios from 'axios';
 
+const GITHUB_API_URL = 'https://api.github.com';
+
+export const fetchUsers = async (searchTerm, location = '', minRepos = '') => {
+  let query = searchTerm;
+
+  if (location) {
+    query += `+location:${location}`;
+  }
+
+  if (minRepos) {
+    query += `+repos:>${minRepos}`;
+  }
+
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/search/users?q=${query}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
