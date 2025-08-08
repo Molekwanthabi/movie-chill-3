44527f7d6a0c0f84e9ab561d28d89033e07de8ac
Nthabiseng <molekwanthabiseng@gmail.com>
2025-08-08 15:06:46 +0200

    Added city

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#e6f7ff',
+        minHeight: '200px',
+      }}
+    >
+      <h2>Main Content</h2>
+      <p>This is where the main content of the app will go.</p>
+    </main>
+  );
+}
+
+export default MainContent;
