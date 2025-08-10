eedd6a4d62cad9ac392ec7c027c223ca7cf8ef9a
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-08-10 12:19:55 +0200

    Update WelcomeMessage.jsx

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -2,7 +2,7 @@ function WelcomeMessage() {
     return (
         <div>
             <h1>Welcome to ALX React APP!</h1>
-            <p>This is a simple JSX component.</p>
+            <p>Hello everyone, I am learning React at ALX!"</p>
         </div>
     );
 }
