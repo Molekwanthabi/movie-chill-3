da5d4d578881416212c8f3ce455920ff65b499fd
Nthabiseng <molekwanthabiseng@gmail.com>
2025-08-28 14:06:53 +0200

    initial project files

--- movie-chill-2/src/pages/SearchBar.js
+++ movie-chill-2/src/pages/SearchBar.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { apikey } from "../API";
+
+
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = ("");
+  const [data, setData] = useState({});
   const onSearchHandler = () => {
-    alert(123);
+    if(!searchTerm) {
+      return;
+    }
+    axios({
+      method:"GET",
+      url:`http://www.omdbapi.com/?t=${searchTerm}&&apiKey=${apikey}`
+    }).then(response => {
+      console.log(response.data);
+    });
   };
 
   return (
@@ -13,6 +26,8 @@ const SearchBar = () => {
         type="text"
         placeholder="Search for a movie..."
         className="text-[19] mr-4 p-2 w-[30%] border rounded"
+        value={searchTerm}
+        onChange={(e)=>searchTerm(e.target.value)}
       />
       <button type="submit" className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700"onClick={onSearchHandler}
       >
